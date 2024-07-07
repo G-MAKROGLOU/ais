7c61f0e4f747ea64f5a88f1a4c42192283db00a5
George Makroglou <g.makroglou@bpc.gr>
2024-07-07 03:54:16 +0300

    uncommented main.go ~ no routes yet

--- cmd/ais/main.go
+++ cmd/ais/main.go
@@ -1,47 +1,46 @@
 package main
 
 import (
+	"log"
+	"strings"
+
 	"github.com/G-MAKROGLOU/ais/internal/config"
+	"github.com/G-MAKROGLOU/ais/internal/rabbitmq"
+	"github.com/gofiber/fiber/v2"
 )
 
-// ENV is env
 func main() {
 	// load env
-
 	config.LoadEnv()
 
 	// init db
 
 	// init rabbitmq
-	//if err := rabbitmq.RMQConnect(""); err != nil {
-	//	log.Println("RabbitMQ connection error: ", err.Error())
-	//}
-	//defer rabbitmq.RabbitMQ.Close()
+	if err := rabbitmq.RMQConnect(""); err != nil {
+		log.Println("RabbitMQ connection error: ", err.Error())
+	}
+	defer rabbitmq.RabbitMQ.Close()
 
-	//// init app
-	//app := fiber.New(fiber.Config{
-	//	CaseSensitive:     true,
-	//	ETag:              true,
-	//	EnablePrintRoutes: true,
-	//	StrictRouting:     true,
-	//	UnescapePath:      true,
-	//})
+	// init app
+	app := fiber.New(fiber.Config{
+		CaseSensitive:     true,
+		ETag:              true,
+		EnablePrintRoutes: true,
+		StrictRouting:     true,
+		UnescapePath:      true,
+	})
 
-	//// register middlewares
-	//SetupCoreMiddlewares(app)
+	// register middlewares
+	SetupCoreMiddlewares(app)
 
-	//// register routes and route specific middlewares
+	// register routes and route specific middlewares
 
-	//// listen per environment
-	//if ENV == "development" {
-	//	log.Fatalln(app.Listen(":3000"))
-	//}
+	// listen per environment
+	if config.AppConfig.Env == "development" || config.AppConfig.Env == "staging" || strings.TrimSpace(config.AppConfig.Env) == "" {
+		log.Fatalln(app.Listen(config.AppConfig.Port))
+	}
 
-	//if ENV == "staging" {
-	//	log.Fatalln(app.Listen(":80"))
-	//}
-
-	//if ENV == "production" {
-	//	log.Fatalln(app.ListenTLS(":443", "./cert.pem", "./cert.key"))
-	//}
+	if config.AppConfig.Env == "production" {
+		log.Fatalln(app.ListenTLS(":443", "./cert.pem", "./cert.key"))
+	}
 }
